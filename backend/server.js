bf66da7fd0c25e0469f8f5166243ba01c5fe2e86
SumedhGaikwad03 <SumedhGaikwad1203@gmail.com>
2025-08-27 15:54:36 +0530

    7.2

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.post("/api/enquiry", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
